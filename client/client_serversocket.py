431ec21c0aa7cfe597df9be4aba714dff1aae58b
akmalzakia <zakiasmara11@gmail.com>
2022-10-07 19:03:36 +0700

    set dynamic host on client

--- client/client_serversocket.py
+++ client/client_serversocket.py
@@ -15,7 +15,7 @@ BUFFER_SIZE = 1024
 PORT = 5000
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "192.168.1.15"
+SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
